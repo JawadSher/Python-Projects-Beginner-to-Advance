1234aa100ad4d7f8f4374adafd0160fdd9a40bb1
Jawad Sher <jawadsher04@gmail.com>
2024-05-01 18:59:48 +0500

    80% Game is ready

--- Project 1 - Number Guessing Game/main.py
+++ Project 1 - Number Guessing Game/main.py
@@ -1,4 +1,28 @@
 import random
-import math
 
-print(random.randint(0, 10))
+randomNumber = random.randint(0, 99)
+Rounds = int(input("Please Enter number of Rounds: "))
+print("---------------------------------------------")
+
+while(Rounds):
+    userInput = int(input("Please Guess the number : "))
+    if(userInput != randomNumber and  userInput >= randomNumber):
+        print("The userInput is greater than Random Number")
+        print(f"Random Number : {randomNumber}")
+        print(f"userInput Number : {userInput}")
+    elif(userInput != randomNumber  and userInput <= randomNumber):
+        print("The userInput is less than Random Number")
+        print(f"Random Number : {randomNumber}")
+        print(f"userInput Number : {userInput}")
+    else:
+        print("Woooooooh You got the Number")
+        print(f"Random Number : {randomNumber}")
+        print(f"userInput Number : {userInput}")
+        break
+
+    Rounds = Rounds - 1
+    print(f"Number of Rounds Remaining : {Rounds}")
+    print("----------------------------------------")
+
+if(Rounds==0 & userInput != randomNumber):
+    print("Try Next Time")
