c4ceebb1e24a880a3c317a3622ac12e6baa4e759
Jawad Sher <jawadsher04@gmail.com>
2024-05-03 18:55:31 +0500

    100% Completed

--- Project 1 - Number Guessing Game/main.py
+++ Project 1 - Number Guessing Game/main.py
@@ -1,28 +1,42 @@
 import random
+import time
 
 randomNumber = random.randint(0, 99)
 Rounds = int(input("Please Enter number of Rounds: "))
-print("---------------------------------------------")
 
-while(Rounds):
-    userInput = int(input("Please Guess the number : "))
-    if(userInput != randomNumber and  userInput >= randomNumber):
-        print("The userInput is greater than Random Number")
-        print(f"Random Number : {randomNumber}")
-        print(f"userInput Number : {userInput}")
-    elif(userInput != randomNumber  and userInput <= randomNumber):
-        print("The userInput is less than Random Number")
-        print(f"Random Number : {randomNumber}")
-        print(f"userInput Number : {userInput}")
-    else:
-        print("Woooooooh You got the Number")
-        print(f"Random Number : {randomNumber}")
-        print(f"userInput Number : {userInput}")
-        break
+if(Rounds==0):
+    print("Enter Valid Number of Rounds")
+else:
+    print("---------------------------------------------")
+    print("Count Down Start .....")
+    countDown = 3
+    while(countDown):
+        mins, secs = divmod(countDown, 60)
+        timeFormat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeFormat, end='\r')
+        time.sleep(1)
+        countDown -= 1
 
-    Rounds = Rounds - 1
-    print(f"Number of Rounds Remaining : {Rounds}")
-    print("----------------------------------------")
+    while(Rounds):
+        userInput = int(input("Please Guess the number : "))
+        if(userInput != randomNumber and  userInput >= randomNumber):
+            print("The userInput is greater than Random Number")
+            print(f"Random Number : {randomNumber}")
+            # print(f"userInput Number : {userInput}")
+        elif(userInput != randomNumber  and userInput <= randomNumber):
+            print("The userInput is less than Random Number")
+            print(f"Random Number : {randomNumber}")
+            # print(f"userInput Number : {userInput}")
+        else:
+            print("---> Nice You got the Number <---")
+            print(f"Random Number : {randomNumber}")
+            print(f"userInput Number : {userInput}")
+            break
 
-if(Rounds==0 & userInput != randomNumber):
-    print("Try Next Time")
+        Rounds = Rounds - 1
+        print(f"**** Number of Rounds Remaining : {Rounds} ****")
+        print("----------------------------------------")
+        print("Try next time") if(Rounds == 0) else ""
+
+
+
