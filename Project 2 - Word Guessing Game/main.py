8652f4ec08412d7f1581c1949e271432ea6bc842
Jawad Sher <jawadsher04@gmail.com>
2024-05-19 16:59:59 +0500

    adding second project

---
+++ Project 2 - Word Guessing Game/main.py
@@ -0,0 +1,9 @@
+import random
+
+userName = input("Please Enter Your Name : ")
+print(f'Welcome Mr. {userName}')
+
+words = ['rainbow', 'butterfly', 'windows', 'programming', 'python', 'geekforgeeks', 'mathematics', 'amazing', 'alon']
+
+word = random.choice(words)
+print(word)
