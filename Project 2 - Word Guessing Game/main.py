c84e9851596df917beb975f1446a36dae1f2a936
Jawad Sher <jawadsher04@gmail.com>
2024-05-19 17:27:48 +0500

    nothing special right now

--- Project 2 - Word Guessing Game/main.py
+++ Project 2 - Word Guessing Game/main.py
@@ -1,10 +1,2 @@
-import random
 
-def display_words(secret_words, guessed_words):
-    for word in secret_words:
-        if word in guessed_words:
-            print(word, end=" ")
-        else:
-            print("_", end=" ")
-
-
+    
