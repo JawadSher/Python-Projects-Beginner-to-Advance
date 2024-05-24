a30d428ac97a6c7824b312c6b5b9e525cb4dce9c
Jawad Sher <jawadsher04@gmail.com>
2024-05-24 17:41:35 +0500

    Adding third Project

--- Project 3 - Hungman Game/main.py
+++ Project 3 - Hungman Game/main.py
@@ -1,70 +1,61 @@
 import random
-from collections import Counter 
+from collections import Counter
 
-fruits = '''apple banana mango strawberry  
-orange grape pineapple apricot lemon coconut watermelon 
-cherry papaya berry peach lychee muskmelon'''
-
-animals = '''tiger lion elephant leopord gorilla hyena panther camel alligator panda mole'''
+fruits = '''mango banana strawbarry apple leamon tomato'''
+animals = '''tiger lion chita rabit snake elephant giraffe'''
 
 fruitList = fruits.split()
 animalList = animals.split()
 
-word_category = {word:'fruit' for word in fruitList}
-word_category.update({word:'animal' for word in animalList})
+word_Category = {word:'fruit' for word in fruitList}
+word_Category.update({word: 'animal' for word in animalList})
 
 wordList = fruitList + animalList
 word = random.choice(wordList)
-category = word_category[word]
+category = word_Category[word]
 print(word)
-if __name__ == '__main__' :
-	print(f'Guess the Word! : Hint -> Category {category}')
-
+if __name__ == '__main__':
+	print(f'Guess the word! : Hint - Category {category}')
+	
+	for i in word:
+		print("_", end="")
+		print()
+	
 	rounds = len(word) + 2
-	print(f'------> Total Number of Rounds {rounds} <------')
-	letterGuessed = ''
-	correct = 0
 	flag = 0
+	letterGuessed = ''
 
+	print(f'---> Total Number of Rounds : {rounds} <---')
 	try:
-		for i in word:
-			print("_", end=" ")
-			print()
 		while(rounds and flag == 0):
-			guess = input("Enter a letter : ")
-			if not guess.isalpha():
-				print("Only Letters are Allowed !!!")
-				continue
+			guess = input("Inter a letter : ")
+			if not guess.isalpha:
+				print("Only Alhpabetic Character Allowed")
 			if len(guess) != 1:
-				print("Please Enter only Once Letter at a time")
-				continue
-
+				print("Only Once Character Allowed at a Time")
 			if guess in word:
-				c = word.count(guess)
-				for _ in range(c):
+				w = word.count(guess)
+				for _ in range(w):
 					letterGuessed += guess
-
 			for char in word:
 				if char in letterGuessed and (Counter(letterGuessed) != Counter(word)):
 					print(char, end=" ")
-					correct += 1
-				elif (Counter(letterGuessed) == Counter(word)):
-					print(f'The word is : ', end=" ")
-					print(word)
+				elif(Counter(letterGuessed) == Counter(word)):
 					flag = 1
-					print("Congratulations ***")
+					print(f'The word is : {word}')
+					print("*** Congratulations ***")
 					break
 					break
 				else:
-					print('_', end=" ")
+					print("_", end=" ")
 			rounds -= 1
 
 		if rounds <= 0 and (Counter(letterGuessed) != Counter(word)):
 			print()
-			print("You Lost the Game !!!")
-			print('The word is {}'.format(word))
+			print("You Lose the Game")
+			print(f'The word is {word}')
 
 	except KeyboardInterrupt:
 		print()
-		print("Bye! Try Again...")
+		print("Bye! Try Next Time")
 		exit()
