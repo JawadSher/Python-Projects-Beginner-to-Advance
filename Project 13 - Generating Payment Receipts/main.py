5337abaa5d32f24ca90a7be5f4b793571b70b3c9
Jawad Sher <jawadsher04@gmail.com>
2024-07-08 21:10:39 +0500

    code Updated

--- Project 13 - Generating Payment Receipts/main.py
+++ Project 13 - Generating Payment Receipts/main.py
@@ -3,6 +3,7 @@ from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet
 from datetime import datetime
+import win10toast
 
 def store_products():
     print('=====> WELCOME TO MY-SCHOOL <=====')
@@ -52,6 +53,8 @@ def store_products():
 
     parchasing = True
 
+    totalFee = 0
+    discount = 3000
     while(parchasing):
         buy_course = int(input("Which Course Do you wan to Buy (0, 1, 2....) : "))
         if(buy_course > len(default_courses)):
@@ -75,13 +78,54 @@ def store_products():
                 course['Price']
             ])
 
+            if course['Price'] != 'Free':
+                totalFee += course['Price']
+            
+
             for row in templete:
                 print(row)
-        exit = input("Do you want to exit (0 - exit) : ")
+
+        exit = input("\nDo you want to exit (0 - exit - No) : ")
         if(exit == 'exit' or exit == '0'):
             parchasing = False
-        
+    
+    
+    templete.append([
+        ("Discount"),
+        (""),
+        (""),
+        (""),
+        (discount),
+    ])
+    templete.append([
+        ("Total"),
+        (""),
+        (""),
+        (""),
+        (totalFee-discount),
+    ])
 
-
-
+    pdf = SimpleDocTemplate('Receipt.pdf', pagesize = A4)
+    toaster = win10toast.ToastNotifier()
+    t_title = "Courses Reciept Ready!"
+    message = "Courses has been parchased from My-School"
+    toaster.show_toast(t_title, message)
+    styles = getSampleStyleSheet()
+    title_style = styles['Heading1']
+    title_style.alignment = 1
+    title = Paragraph('My-School Courses', title_style)
+    style = TableStyle(
+        [
+            ( "BOX" , ( 0, 0 ), ( -1, -1 ), 1 , colors.black ), 
+            ( "GRID" , ( 0, 0 ), ( 4 , 4 ), 1 , colors.black ), 
+            ( "BACKGROUND" , ( 0, 0 ), ( 3, 0 ), colors.gray ), 
+            ( "TEXTCOLOR" , ( 0, 0 ), ( -1, 0 ), colors.black ), 
+            ( "BACKGROUND" , ( 0, 0 ), ( 4, 0 ), colors.gray ), 
+            ( "ALIGN" , ( 0, 0 ), ( -1, -1 ), "CENTER" ), 
+            ( "BACKGROUND" , ( 0 , 1 ) , ( -1 , -1 ), colors.beige ), 
+            ("BACKGROUND", (0, -2), (-1, -1), colors.lightblue), 
+        ]
+    )
+    table = Table(templete, style=style)
+    pdf.build([title, table])
 store_products()
